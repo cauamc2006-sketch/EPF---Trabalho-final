6af4d0d9101983363fda5a8e4e65621bfc239970
Caua Mendes Coelho <cauamc2006@gmail.com>
2025-11-19 19:30:08 -0300

    Criei a classe pedido

--- models/Carrinho.py
+++ models/Carrinho.py
@@ -3,42 +3,42 @@ from Jogo import Jogo
 class Carrinho:
     
     def __init__(self):
-        self.itens = []
+        self.__itens = []
         
-        
+    def get_itens(self):
+        return self.__itens
+    
+    def get_total(self):
+        return sum(item.get_preco() for item in self.__itens)
+    
     def adicionar_jogo(self, jogo: Jogo):
-        self.itens.append(jogo)
-        print(f"Jogo {jogo.nome} adicionado na sua lista!!")
+        self.get_itens().append(jogo)
+        print(f"Jogo {jogo.get_nome()} adicionado no seu carrinho!")
         
     
     def exibir_lista(self):
         for jogo in self.itens:
-            print(jogo.nome)
+            print(jogo.get_nome())
         print("--------")
      
         
     def remover_jogo(self, jogo: Jogo):
-        self.itens.remove(jogo)
-        print(f"Jogo {jogo.nome} removido de sua lista!")
+        self.__itens.remove(jogo)
+        print(f"Jogo {jogo.get_nome()} removido do seu carrinho!")
         
 
-    def calcular_total(self):
-        total = 0
-        for jogo in self.itens:
-            total += jogo.preco
-            
-        print(f"Total: R${total:.2f}")
+  
         
-j1 = Jogo(3, "Fortnite", 200, "Battle royale")
-j2 = Jogo(2,"Resident Evil", 200 , "terror")
+#j1 = Jogo(3, "Fortnite", 200, "Battle royale")
+#j2 = Jogo(2,"Resident Evil", 200 , "terror")
 
-c1 = Carrinho()
+#c1 = Carrinho()
 
-c1.adicionar_jogo(j1)
-c1.adicionar_jogo(j2)
-c1.exibir_lista()
+#c1.adicionar_jogo(j1)
+#c1.adicionar_jogo(j2)
+#c1.exibir_lista()
 
-c1.calcular_total()
+#c1.calcular_total()
        
        
     
--- models/jogo.py
+++ models/jogo.py
@@ -1,22 +1,29 @@
 class Jogo:
    
     def __init__(self, id, nome, preco, genero):
-        self.id = id
-        self.nome = nome
-        self.preco = preco
-        self.genero = genero
+        self.__id = id
+        self.__nome = nome
+        self.__preco = preco
+        self.__genero = genero
+    
+    def get_preco(self):
+        return self.__preco
+    
+    def get_nome(self):
+        return self.__nome
     
     
     def atualizar_preco(self,novo_preco: float):
-        if (novo_preco == self.preco):
-            print("Digite um preço diferente do atual!!!")
+        if (novo_preco == self.get_preco()):
+            print("Digite um preço diferente do atual!")
         else:
             self.preco = novo_preco
-            print(f"Preço atualizado com sucesso!!!, seu preço agora é R${novo_preco}")  
+            print(f"Preço atualizado com sucesso!, seu preço agora é R${novo_preco:.2f}")  
 
 
 j3 = Jogo(2,"Resident-Eviel", 200 , "terror")
 
+#j3.atualizar_preco(350)
 #print(j1.preco)
 
 
